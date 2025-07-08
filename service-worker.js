ddcd79dcb30d20ab3f52fde3ae72ddeb7dfcf959
Hassan-nizami-coding <hassannzami1@gmail.com>
2025-07-09 00:33:43 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,19 @@
+const CACHE_NAME = "blind-aid-cache-v1";
+const urlsToCache = [
+  "/blindAid/index.html",
+  "/blindAid/lib/tf.min.js",
+  "/blindAid/lib/coco-ssd.min.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
 
